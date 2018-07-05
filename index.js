9d8aa8a6b4898bca2ea35492900c6d34c68ec838
kosatun@aol.com <kosatun@aol.com>
2018-07-05 18:27:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,21 +10,73 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+    item = getCartItem(item);
+    getCart().push(item);
+
+    return `${item.itemName} has been added to your cart.`;
+   }
+   
+function getCartItem(itemName) {
+  return {
+      itemName: itemName,
+      price: getPrice(1, 100)
+    };
+}
+
+function getPrice(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 function viewCart() {
-  // write your code here
+  if (getCart() === '' ) {
+   return  "Your shopping cart is empty.";
+  }   
+  return getCartDescription();
+}  
+ 
+function getCartDescription() {
+  var cartDescription = 'In your cart, you have ';
+  if ( getCart().length >= 1 ) {
+    cartDescription += `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`;
+  }
+  if ( getCart().length >= 2 ) {
+    var midCartDescription = '';
+    for (var i=1; i<getCart().length -1; i++) {
+      midCartDescription += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`;
+    }
+    cartDescription += `${midCartDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`;
+  }
+
+  return `${cartDescription}.`;
 }
 
 function total() {
-  // write your code here
+  var itemTotal = 0;
+  for (var i = 0; i < getCart().length; i++) {
+    itemTotal = itemTotal + getCart()[i].price;
+  }
+    return itemTotal;
 }
 
-function removeFromCart(item) {
-  // write your code here
+function removeFromCart(itemName) {
+  for (var i = 0; i < getCart().length; i++){
+    var delItem;
+    if (getCart()[i].itemName === itemName) {
+      delItem = Array.prototype.splice(getCart()[i].itemName, 1);
+      return getCart();
+    } 
+    if (getCart()[i].itemName !== itemName) {
+      return "That item is not in your cart.";
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber) {
+    setCart([]);
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+  }
+  if (arguments.length === 0) {
+    return "Sorry, we don't have a credit card on file for you.";
+  }
 }
